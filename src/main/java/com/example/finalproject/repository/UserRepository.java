d28f8ce804199abdf4ff8035e3684ca16446e0c3
Ivan Marinov <vando1661@gmail.com>
2023-11-17 00:34:15 +0000

    Create UserRepository.java

---
+++ src/main/java/com/example/finalproject/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.example.finalproject.repository;
+
+import com.example.finalproject.model.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsernameAndPassword(String username, String password);
+
+}
