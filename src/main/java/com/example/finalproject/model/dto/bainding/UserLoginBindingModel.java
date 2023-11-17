3ac4bdd3ee754d8b127f5849b27792b3d7d2c8cb
Ivan Marinov <vando1661@gmail.com>
2023-11-17 00:34:51 +0000

    Create UserLoginBindingModel.java

---
+++ src/main/java/com/example/finalproject/model/dto/bainding/UserLoginBindingModel.java
@@ -0,0 +1,31 @@
+package com.example.finalproject.model.dto.bainding;
+
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+
+public class UserLoginBindingModel {
+    @Size(min = 5, max = 20)
+    private String username;
+
+    @Size(min = 3)
+    private String password;
+
+    public UserLoginBindingModel() {
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
