6dda7f0bb9b25dee23694b000691679f19195864
Ivan Marinov <vando1661@gmail.com>
2023-12-06 04:46:54 +0000

    Update RoleServiceImpl.java

--- src/main/java/com/example/finalproject/service/impl/RoleServiceImpl.java
+++ src/main/java/com/example/finalproject/service/impl/RoleServiceImpl.java
@@ -1,22 +1,48 @@
 package com.example.finalproject.service.impl;
 
-import com.example.finalproject.model.entity.RoleUserEntity;
+import com.example.finalproject.model.entity.RoleEntity;
 import com.example.finalproject.model.enums.RoleUserEnum;
 import com.example.finalproject.repository.RoleRepository;
+import com.example.finalproject.sec.CurrentUser;
 import com.example.finalproject.service.RoleService;
 import org.springframework.stereotype.Service;
 
+import java.util.Arrays;
 
 @Service
 public class RoleServiceImpl implements RoleService {
-    private final RoleRepository roleRepository;
 
-    public RoleServiceImpl(RoleRepository roleRepository) {
+    private final RoleRepository roleRepository;
+    private final CurrentUser currentUser;
+
+    public RoleServiceImpl(RoleRepository roleRepository, CurrentUser currentUser) {
         this.roleRepository = roleRepository;
+        this.currentUser = currentUser;
     }
 
     @Override
-    public RoleUserEntity getRoleByName(String name) {
-        return this.roleRepository.findByName(RoleUserEnum.valueOf(name));
+    public void initRoles() {
+
+        if(roleRepository.count() != 0){
+            return;
+        }
+        Arrays.stream(RoleUserEnum.values())
+                .forEach(roleUserEnum -> {
+                    RoleEntity roleEntity = new RoleEntity();
+                    roleEntity.setRole(roleUserEnum);
+//                    if(currentUser.getId() == 1){
+//                        roleEntity.setRole(RoleUserEnum.ADMIN);
+//                    }else {
+//                        roleEntity.setRole(RoleUserEnum.USER);
+//                    }
+                    roleRepository.save(roleEntity);
+                });
+    }
+
+    @Override
+    public RoleEntity findByRoleUserEnum(RoleUserEnum roleUserEnum) {
+        return roleRepository
+                .findByRole(roleUserEnum)
+                .orElse(null);
     }
 }
