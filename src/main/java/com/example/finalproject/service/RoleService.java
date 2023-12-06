019122e8570c77089e5e13f6cacbad52a54e7618
Ivan Marinov <vando1661@gmail.com>
2023-12-06 04:47:01 +0000

    Update RoleService.java

--- src/main/java/com/example/finalproject/service/RoleService.java
+++ src/main/java/com/example/finalproject/service/RoleService.java
@@ -1,8 +1,11 @@
 package com.example.finalproject.service;
 
-
-import com.example.finalproject.model.entity.RoleUserEntity;
+import com.example.finalproject.model.entity.RoleEntity;
+import com.example.finalproject.model.enums.RoleUserEnum;
 
 public interface RoleService {
-    RoleUserEntity getRoleByName(String name);
+
+    void initRoles();
+
+    RoleEntity findByRoleUserEnum(RoleUserEnum roleUserEnum);
 }
