2d82f0e33ece9111abe51fc52cee6b3ab0a96114
Ivan Marinov <vando1661@gmail.com>
2023-12-06 04:47:12 +0000

    Create data.sql

---
+++ src/main/resources/data.sql
@@ -0,0 +1,14 @@
+INSERT INTO users (email, name, password, surname, username)
+VALUES
+    ('van@do.com','ivan','1234','marinov','vando2'),
+    ('vando@do.com','ivan','1234','marinov','vando3');
+#
+# INSERT INTO roles (`id`, `role`)
+# VALUES
+#     (1, 'ADMIN'),
+#     (2, 'USER');
+#
+# INSERT INTO users_roles(`user_id`, `role_id`)
+# VALUES
+#     (1, 1)
+#    (1, 2);
