6266aa5dc6c89a9cbd18bbaa57c4ab19744d7e30
Ivan Marinov <vando1661@gmail.com>
2023-12-06 04:46:35 +0000

    Create PlanRepository.java

---
+++ src/main/java/com/example/finalproject/repository/PlanRepository.java
@@ -0,0 +1,14 @@
+package com.example.finalproject.repository;
+
+import com.example.finalproject.model.entity.PlanEntity;
+import com.example.finalproject.model.enums.PlanEnum;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+@Repository
+public interface PlanRepository extends JpaRepository<PlanEntity,Long> {
+
+
+Optional<PlanEntity> findByPlan(PlanEnum planEnum);
+}
