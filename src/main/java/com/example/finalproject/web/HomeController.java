56420e1b73d4ae7006d96b8f93adfbfc5bf93f25
Ivan Marinov <vando1661@gmail.com>
2023-12-06 04:47:06 +0000

    Update HomeController.java

--- src/main/java/com/example/finalproject/web/HomeController.java
+++ src/main/java/com/example/finalproject/web/HomeController.java
@@ -1,13 +1,52 @@
 package com.example.finalproject.web;
 
+import com.example.finalproject.sec.CurrentUser;
+import com.example.finalproject.service.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
-    @GetMapping()
-    public String index(){
-        return "index";
+    private final CurrentUser currentUser;
+    private final UserService userService;
+
+    public HomeController(CurrentUser currentUser, UserService userService) {
+        this.currentUser = currentUser;
+        this.userService = userService;
+    }
+
+    @GetMapping("/home")
+    public String home() {
+        return "/index";
+    }
+
+    @GetMapping("/about")
+    public String about(){
+        return "/about";
+    }
+    @GetMapping("/healthy-food")
+            public String healthyFood(){
+        return "/healthy-food";
+    }
+    @GetMapping("/kids-zone")
+    public String kidsZone(){
+        return "/kids-zone";
+    }
+    @GetMapping("/tennis")
+    public String tennis(){
+        return "/tennis";
+    }
+    @GetMapping("/swimming")
+    public String swimming(){
+        return "/swimming";
+    }
+    @GetMapping("/fitness")
+    public String fitness(){
+        return "fitness";
+    }
+    @GetMapping("/gymnastics")
+    public String gymnastics(){
+        return "/gymnastics";
     }
 
 }
