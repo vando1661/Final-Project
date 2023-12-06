4909b8b22e847380bbf2091414573c89d9d2bb3e
Ivan Marinov <vando1661@gmail.com>
2023-12-06 04:46:38 +0000

    Update RoleRepository.java

--- src/main/java/com/example/finalproject/repository/RoleRepository.java
+++ src/main/java/com/example/finalproject/repository/RoleRepository.java
@@ -1,12 +1,13 @@
 package com.example.finalproject.repository;
 
-import com.example.finalproject.model.entity.RoleUserEntity;
+import com.example.finalproject.model.entity.RoleEntity;
 import com.example.finalproject.model.enums.RoleUserEnum;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 
 @Repository
-public interface RoleRepository extends JpaRepository<RoleUserEntity, Long> {
-    RoleUserEntity findByName(RoleUserEnum roleEnum);
+public interface RoleRepository extends JpaRepository<RoleEntity,Long> {
+    Optional<RoleEntity>findByRole(RoleUserEnum roleUserEnum);
 }
