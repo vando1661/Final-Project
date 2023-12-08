800e5e45560efeeef3debd640f65d4a4e4225c66
Ivan Marinov <vando1661@gmail.com>
2023-12-08 09:30:43 +0000

    Update PlanService.java

--- src/main/java/com/example/finalproject/service/PlanService.java
+++ src/main/java/com/example/finalproject/service/PlanService.java
@@ -4,7 +4,17 @@ import com.example.finalproject.model.entity.PlanEntity;
 import com.example.finalproject.model.enums.PlanEnum;
 
 public interface PlanService {
+
     void initPlan();
 
     PlanEntity findByPlanEnum(PlanEnum planEnum);
+
+    PlanEntity getPlanById(Long id);
+
+    PlanEntity getPlanEntityDetails();
+
+
+    void savePlan(PlanEntity planEntity);
 }
+
+
