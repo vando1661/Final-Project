8351bf8781eef3f20dc85a78a8aaaff0672df7ed
Ivan Marinov <vando1661@gmail.com>
2023-12-08 09:30:29 +0000

    Update PlanEnum.java

--- src/main/java/com/example/finalproject/model/enums/PlanEnum.java
+++ src/main/java/com/example/finalproject/model/enums/PlanEnum.java
@@ -1,17 +1,7 @@
 package com.example.finalproject.model.enums;
 
 public enum PlanEnum {
-    BASIC ("BASIC"),
-    STANDARD("STANDARD"),
-    PREMIUM("PREMIUM");
-
-    private final String value;
-
-    PlanEnum(String value) {
-        this.value = value;
-    }
-
-    public String getValue() {
-        return value;
-    }
+    BASIC ,
+    STANDARD,
+    PREMIUM
 }
