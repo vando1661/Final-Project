be84183d070499575cb5ebd64f9c6f8a50c389b3
Ivan Marinov <vando1661@gmail.com>
2023-12-08 09:30:45 +0000

    Update UserService.java

--- src/main/java/com/example/finalproject/service/UserService.java
+++ src/main/java/com/example/finalproject/service/UserService.java
@@ -2,6 +2,7 @@ package com.example.finalproject.service;
 
 import com.example.finalproject.model.entity.UserEntity;
 import com.example.finalproject.model.service.UserServiceModel;
+import org.apache.catalina.User;
 
 public interface UserService {
     UserServiceModel registerUser(UserServiceModel userServiceModel);
@@ -10,9 +11,10 @@ public interface UserService {
 
     void loginUser(Long id, String username);
 
-    UserEntity findById(Long id);
 
+    UserEntity findById(Long id);
 
     void isPlan();
 
+    UserEntity getUserByUsername(String username);
 }
