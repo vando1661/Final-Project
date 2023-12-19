21dab04bc7a9cbbdaf545cf7dc0241b8e5ed6318
Ivan Marinov <vando1661@gmail.com>
2023-12-19 11:48:58 +0000

    Update PlanController.java

--- src/main/java/com/example/finalproject/web/PlanController.java
+++ src/main/java/com/example/finalproject/web/PlanController.java
@@ -2,21 +2,13 @@ package com.example.finalproject.web;
 
 import com.example.finalproject.model.entity.PlanEntity;
 import com.example.finalproject.model.entity.UserEntity;
-import com.example.finalproject.model.enums.PlanEnum;
-import com.example.finalproject.repository.PlanRepository;
 import com.example.finalproject.sec.CurrentUser;
 import com.example.finalproject.service.PlanService;
 import com.example.finalproject.service.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
-import java.awt.*;
-import java.security.Principal;
-import java.util.Arrays;
 import java.util.List;
 
 @Controller
@@ -28,45 +20,45 @@ public class PlanController {
     private final UserService userService;
     private final CurrentUser currentUser;
     private final PlanService planService;
-    private final PlanRepository planRepository;
 
 
-
-    public PlanController(UserService userService, CurrentUser currentUser, PlanService planService, PlanRepository planRepository) {
+    public PlanController(UserService userService, CurrentUser currentUser, PlanService planService) {
         this.userService = userService;
         this.currentUser = currentUser;
         this.planService = planService;
-        this.planRepository = planRepository;
+
     }
 
     @GetMapping("/plans")
-    public String plans(Model model){
-
+    public String showPlans(Model model) {
 
         List<PlanEntity> list = planService.getAllPlans();
 
-        model.addAttribute("currentUser",currentUser.getUsername());
-        model.addAttribute("list",list);
+        model.addAttribute("currentUser", currentUser.getId());
+        model.addAttribute("list", list);
         return "plans";
     }
 
     @PostMapping("/savePlan")
-    public  String savePlan(@RequestParam("userId")Long id,@RequestParam("plansId") Long plansId){
-        Long userLogged = currentUser.getId();
+    public String savePlan(@RequestParam("userId") Long userId, @RequestParam("plansId") Long plansId, Model model) {
+
+        UserEntity user = userService.getUserById(userId);
         PlanEntity plan = planService.getPlanById(plansId);
 
-        planService.savePlan(plan);
-        return "redirect:/profile";
-    }
-    @GetMapping("/profile")
-    public String profile(Model model){
 
-        String userEmail = currentUser.getEmail();
-        model.addAttribute("userEmail", userEmail);
-        if (!model.containsAttribute("isPlan")) {
-            currentUser.setPlan(false);
-            return "/profile";
-        }
-        return "plans";
+        user.setPlan(plan);
+        userService.saveUser(user);
+        planService.savePlan(plan);
+        model.addAttribute("selectedPlan", plan);
+        return "redirect:/profile";
+
+    }
+
+    @GetMapping("/profile")
+    public String showProfile(Model model) {
+
+        PlanEntity selectedPlan = planService.getSelectedPlanForUser(currentUser.getId());
+        model.addAttribute("selectedPlan",selectedPlan);
+        return "profile";
     }
 }
